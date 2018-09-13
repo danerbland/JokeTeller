6c397b6783e7d72a3668410ff85ca534e5993120
danerbland <danerbland@gmail.com>
2018-09-13 19:56:54 -0400

    Added verifyAsyncTask Test

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/MainActivityTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/MainActivityTest.java
@@ -1,12 +1,26 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.util.Log;
+import android.util.Pair;
+
+import com.example.android.androidjokelib.JokeActivity;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+import com.udacity.gradle.builditbigger.backend.myApi.MyApi;
 
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.io.IOException;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
@@ -27,5 +41,53 @@ public class MainActivityTest {
     public void verifyJokeResponse(){
         onView(withId(R.id.button_tell_joke)).perform(click());
         onView(withId(R.id.textview_joke)).check(matches(not(withText(""))));
+    }
+
+    @Test
+    public void verifyAsyncTaskResponse(){
+        EndpointsAsyncTask mTask = new EndpointsAsyncTask();
+        assert(mTask.doInBackground(new Pair(this, "")).equalsIgnoreCase("Why does Norway put bar codes on their ships?" + "\n\n" + "So that they can Scandanavian"));
+    }
+
+
+    public class EndpointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, String> {
+        private MyApi myApiService = null;
+        private Context context;
+        public String mResult;
+
+        @Override
+        protected String doInBackground(Pair<Context, String>... params) {
+            if(myApiService == null) {  // Only do this once
+                MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                        new AndroidJsonFactory(), null)
+                        // options for running against local devappserver
+                        // - 10.0.2.2 is localhost's IP address in Android emulator
+                        // - turn off compression when running against local devappserver
+                        .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                        .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                            @Override
+                            public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) {
+                                abstractGoogleClientRequest.setDisableGZipContent(true);
+                            }
+                        });
+                // end options for devappserver
+
+                myApiService = builder.build();
+            }
+
+            context = params[0].first;
+            String name = params[0].second;
+
+            try {
+                return myApiService.tellJoke().execute().getData();
+            } catch (IOException e) {
+                return e.getMessage();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            return;
+        }
     }
 }
